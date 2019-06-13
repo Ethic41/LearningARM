544ea522ac9c113eba78f4899755f2fbf0ab4b92
ethic41 <dahirmuhammad3@gmail.com>
2019-06-13 23:58:24 +0100

    updated sample_1.s

--- example_1.s
+++ example_1.s
@@ -1,1 +1,14 @@
-add r1, r
+@ Author: Dahir Muhammad Dahir
+@ Date: 13th-June-2019 10:40 PM
+@ About: I will tell u later
+@ example_1.s
+
+
+start:
+add 	r0, r0, #2  	// Add immediate: r0 += 2
+add 	r0, r0, r1  	// Add registers: r0 += r1
+add 	r0, r1, r1  	// Add registers: r0 = r1 + r2
+
+ldr 	r0, [r2]		// Load int from memory: r0 = r2[0]
+ldr 	r0, [r2, #4]	// Load int from memory: r0 = r2[1]
+ldmia	r2, {r0, r1}	// Load mulitple: r0 = r2[0]; r1 = r2[1]
