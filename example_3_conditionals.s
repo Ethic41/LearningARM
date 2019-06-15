d602ccf9e2f53b096e17425405e2127fcc3c23b8
ethic41 <dahirmuhammad3@gmail.com>
2019-06-15 07:04:41 +0100

    file:added conditionals

---
+++ example_3_conditionals.s
@@ -0,0 +1,18 @@
+@ Author: Dahir Muhammad Dahir
+@ Date: 14th-June-2019 06:27 AM
+@ About: I will tell u later
+@ example_3_conditionals.s
+
+
+// Traditional Code
+start:
+		cmp		r0, r1		// perform test here
+		blt		.lbmax		// branch
+		mov		r2, r0		// r0 is higher
+		b 		.lrest		// continue the rest, skipping r1 is higher
+
+.lbmax:
+		mov		r2, r1		// r1 is higher
+
+.lrest:
+		...
